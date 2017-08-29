22f986d8707f2680a304067d964218bd189fc405
Pauline <pauline.design007@gmail.com>
2017-08-29 19:54:39 +1200

    API difficulties

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function myMap()
 {
-  myCenter=new google.maps.LatLng(41.2865, -174.7762);
+  myCenter=new google.maps.LatLng(41.878114, -87.629798);
   var mapOptions= {
     center:myCenter,
     zoom:12, scrollwheel: false, draggable: false,
